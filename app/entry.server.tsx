7fd1d118f1eb7de79cd3be2a5bcf62bb98323145
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-16 19:20:34 +0900

    chore: configure coop/coep header on server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -13,6 +13,8 @@ const handler: HandleDocumentRequestFunction = (
     <RemixServer context={remixContext} url={request.url} />
   );
   responseHeaders.set("content-type", "text/html");
+  responseHeaders.set("cross-origin-opener-policy", "same-origin");
+  responseHeaders.set("cross-origin-embedder-policy", "require-corp");
   return new Response("<!DOCTYPE html>" + html, {
     status: responseStatusCode,
     headers: responseHeaders,
